d3384972cf04f0f3e13a4c9b4a0023f017691b4d
monkey <monkey.d.luffer@gmail.com>
2017-02-22 18:08:38 +0800

    增加后台关闭直播间的接口

---
+++ src/main/java/com/ttpod/star/web/api/notify/GameService.java
@@ -0,0 +1,63 @@
+package com.ttpod.star.web.api.notify;
+
+import com.ttpod.rest.AppProperties;
+import com.ttpod.rest.common.util.JSONUtil;
+import com.ttpod.star.common.util.HttpClientUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Map;
+
+/**
+ * 游戏推送
+ */
+public class GameService {
+
+    static final Logger logger = LoggerFactory.getLogger(GameService.class);
+    // 游戏服务器服务器地址
+    static final String SERVER_DOMAIN = AppProperties.get("aigd.domain");
+
+    /**
+     * 开始游戏
+     * @param roomId
+     * @param gameId
+     * @param liveId
+     * @return
+     */
+    public static Boolean starGame(Object roomId, Object gameId, String liveId) {
+        try {
+            String open_game_server_url = SERVER_DOMAIN + "/api/room/open?room_id="+roomId+"&game_id="+gameId+"&live_id="+liveId;
+            String resp = HttpClientUtils.get(open_game_server_url, null);
+            Map result = JSONUtil.jsonToMap(resp);
+            if (!result.get("code").toString().equals("1")) {
+                return Boolean.FALSE;
+            }
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+            return Boolean.FALSE;
+        }
+        return Boolean.TRUE;
+    }
+
+    /**
+     * 结束游戏
+     * @param roomId
+     * @param gameId
+     * @param liveId
+     * @return
+     */
+    public static Boolean closeGame(Object roomId, Object gameId, String liveId) {
+        try {
+            String open_game_server_url = SERVER_DOMAIN + "/api/room/close?room_id="+roomId+"&game_id="+gameId+"&live_id="+liveId;
+            String resp = HttpClientUtils.get(open_game_server_url, null);
+            Map result = JSONUtil.jsonToMap(resp);
+            if (!result.get("code").toString().equals("1")) {
+                return Boolean.FALSE;
+            }
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+            return Boolean.FALSE;
+        }
+        return Boolean.TRUE;
+    }
+}
