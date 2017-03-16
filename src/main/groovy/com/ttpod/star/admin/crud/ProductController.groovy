4b04e2020c54083fbd06536b301e303e6a26a742
monkey <monkey.d.luffer@gmail.com>
2017-03-16 16:55:11 +0800

    库存类型是int 而不是Str

--- src/main/groovy/com/ttpod/star/admin/crud/ProductController.groovy
+++ src/main/groovy/com/ttpod/star/admin/crud/ProductController.groovy
@@ -26,7 +26,7 @@ class ProductController extends BaseController{
 
 
     @Delegate Crud crud = new Crud(shopMongo.getCollection('products'),Boolean.TRUE,
-            [_id:{productKGS.nextId()},name:Str,in_stock:Str, desc:Str, price:Int,status:Bool,order:Int,delivery_type:Int,
+            [_id:{productKGS.nextId()},name:Str,in_stock:Int, desc:Str, price:Int,status:Bool,order:Int,delivery_type:Int,
             timestamp:Timestamp,last_modify:Timestamp,img_url:Str,money:Int],
             new Crud.QueryCondition(){
                 public DBObject sortby(HttpServletRequest req) {
