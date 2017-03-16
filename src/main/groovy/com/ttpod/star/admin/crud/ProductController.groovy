1d7ee732fd1895b11c61583be60cfc227991967a
monkey <monkey.d.luffer@gmail.com>
2017-03-16 14:07:13 +0800

    product id自增长

--- src/main/groovy/com/ttpod/star/admin/crud/ProductController.groovy
+++ src/main/groovy/com/ttpod/star/admin/crud/ProductController.groovy
@@ -3,12 +3,15 @@ package com.ttpod.star.admin.crud
 import com.mongodb.BasicDBObject
 import com.mongodb.DBObject
 import com.ttpod.rest.anno.RestWithSession
+import com.ttpod.rest.persistent.KGS
 import com.ttpod.rest.web.Crud
 import com.ttpod.star.admin.BaseController
 
+import javax.annotation.Resource
 import javax.servlet.http.HttpServletRequest
 
 import static com.ttpod.rest.groovy.CrudClosures.*
+
 
 /**
  * 2016/10/12
@@ -17,10 +20,12 @@ import static com.ttpod.rest.groovy.CrudClosures.*
 //@Rest
 @RestWithSession
 class ProductController extends BaseController{
+    @Resource
+    KGS productKGS
 
 
     @Delegate Crud crud = new Crud(shopMongo.getCollection('products'),Boolean.TRUE,
-            [_id:Int,name:Str,in_stock:Str, desc:Str, price:Int,status:Bool,order:Int,delivery_type:Int,
+            [_id:{productKGS.nextId()},name:Str,in_stock:Str, desc:Str, price:Int,status:Bool,order:Int,delivery_type:Int,
             timestamp:Timestamp,last_modify:Timestamp,img_url:Str],
             new Crud.QueryCondition(){
                 public DBObject sortby(HttpServletRequest req) {
@@ -28,5 +33,4 @@ class ProductController extends BaseController{
                 }
             }
     )
-
 }
