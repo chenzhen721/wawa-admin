bd16d2816bb09e1ac004c3fff5503aea29af7b23
monkey <monkey.d.luffer@gmail.com>
2017-03-22 18:17:49 +0800

    钻石出入类型格式化

--- src/main/groovy/com/ttpod/star/admin/DiamondController.groovy
+++ src/main/groovy/com/ttpod/star/admin/DiamondController.groovy
@@ -1,12 +1,9 @@
 package com.ttpod.star.admin
 
-import com.mongodb.BasicDBObject
 import com.mongodb.DBObject
-import com.ttpod.rest.anno.Rest
 import com.ttpod.rest.anno.RestWithSession
 import com.ttpod.rest.web.Crud
 import com.ttpod.star.model.DiamondActionType
-import com.ttpod.star.model.OrderType
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.web.bind.ServletRequestUtils
@@ -18,8 +15,8 @@ import static com.ttpod.rest.common.doc.MongoKey.SJ_DESC
 /**
  * 钻石的统计
  */
-@Rest
-//@RestWithSession
+
+@RestWithSession
 class DiamondController extends BaseController {
 
     static final Logger logger = LoggerFactory.getLogger(DiamondController.class)
@@ -28,25 +25,25 @@ class DiamondController extends BaseController {
      * 钻石加币
      * @param req
      */
-    def add_logs(HttpServletRequest req){
-        def userId = ServletRequestUtils.getIntParameter(req,'_id',0)
+    def add_logs(HttpServletRequest req) {
+        def userId = ServletRequestUtils.getIntParameter(req, '_id', 0)
         def query = Web.fillTimeBetween(req)
-        if(userId != 0){
+        if (userId != 0) {
             query.and('user_id').is(userId)
         }
         def diamond_logs = adminMongo.getCollection("diamond_logs")
-        def map =  Crud.list(req, diamond_logs, query.get(), null, SJ_DESC)
+        def map = Crud.list(req, diamond_logs, query.get(), null, SJ_DESC)
         DiamondActionType[] das = DiamondActionType.values()
         def types = [:]
         das.each {
             DiamondActionType d ->
-            types.put(d.actionName,d.name())
+                types.put(d.actionName, d.name())
         }
         def diamondList = map['data'] as List<DBObject>
         diamondList.each {
             DBObject obj ->
                 def type = obj.containsField('type') ? types[obj['type']] : ''
-                obj.put('type',type)
+                obj.put('type', type)
         }
         return map
     }
@@ -55,25 +52,25 @@ class DiamondController extends BaseController {
      * 钻石消费
      * @param req
      */
-    def cost_logs(HttpServletRequest req){
-        def userId = ServletRequestUtils.getIntParameter(req,'_id',0)
+    def cost_logs(HttpServletRequest req) {
+        def userId = ServletRequestUtils.getIntParameter(req, '_id', 0)
         def query = Web.fillTimeBetween(req)
-        if(userId != 0){
+        if (userId != 0) {
             query.and('user_id').is(userId)
         }
         def diamond_cost_logs = adminMongo.getCollection("diamond_cost_logs")
-        def map =  Crud.list(req, diamond_cost_logs, query.get(), null, SJ_DESC)
+        def map = Crud.list(req, diamond_cost_logs, query.get(), null, SJ_DESC)
         DiamondActionType[] das = DiamondActionType.values()
         def types = [:]
         das.each {
             DiamondActionType d ->
-                types.put(d.actionName,d.name())
+                types.put(d.actionName, d.name())
         }
         def diamondList = map['data'] as List<DBObject>
         diamondList.each {
             DBObject obj ->
                 def type = obj.containsField('type') ? types[obj['type']] : ''
-                obj.put('type',type)
+                obj.put('type', type)
         }
         return map
     }
@@ -82,39 +79,24 @@ class DiamondController extends BaseController {
      * 钻石聚合
      * @param req
      */
-    def daily_stat(HttpServletRequest req){
+    def daily_stat(HttpServletRequest req) {
         def query = Web.fillTimeBetween(req)
         def diamond_reports = adminMongo.getCollection('diamond_dailyReport_stat')
-        def map = Crud.list(req, diamond_reports, query.get(), null, SJ_DESC)
+        def data = Crud.list(req, diamond_reports, query.get(), null, SJ_DESC)
         DiamondActionType[] das = DiamondActionType.values()
-        def types = [:]
+        def title = []
+        def column = []
         das.each {
-            DiamondActionType d ->
-                types.put(d.actionName,d.name())
+            title.add(it.name())
+            column.add(it.actionName)
         }
-        def diamondList = map['data'] as List<DBObject>
-        diamondList.each {
-            DBObject obj ->
-                def inc_detail = obj.containsField('inc_detail') ? obj['inc_detail'] as Map : [:]
-                def desc_detail = obj.containsField('desc_detail') ? obj['desc_detail'] as Map : [:]
-                def format_inc_detail = [:]
-                def format_desc_detail = [:]
-                inc_detail.each {
-                    k,v ->
-                        format_inc_detail.put(types[k.toString()],v)
-                }
-                if(!inc_detail.isEmpty()){
-                    obj.put('inc_detail',format_inc_detail)
-                }
-                desc_detail.each {
-                    k,v ->
-                        format_desc_detail.put(types[k.toString()],v)
-                }
-                if(!desc_detail.isEmpty()){
-                    obj.put('desc_detail',format_desc_detail)
-                }
-        }
-        return map
+
+        def map = new HashMap(
+                keys: title,
+                props: column
+        )
+
+        return ['title': map, 'data': data['data']]
     }
 
 
