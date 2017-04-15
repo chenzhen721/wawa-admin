bdc222e6866bf602eb5403260b4a85455b4626f1
monkey <monkey.d.luffer@gmail.com>
2017-04-15 18:17:15 +0800

    红包批量审核通过和拒绝接口

--- src/main/groovy/com/ttpod/star/admin/RedPacketApplyController.groovy
+++ src/main/groovy/com/ttpod/star/admin/RedPacketApplyController.groovy
@@ -10,6 +10,8 @@ import com.ttpod.rest.web.StaticSpring
 import com.ttpod.star.admin.BaseController
 import com.ttpod.star.admin.Web
 import com.ttpod.star.model.ApplyType
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.data.mongodb.core.MongoTemplate
 import org.springframework.web.bind.ServletRequestUtils
 import static com.ttpod.rest.common.util.WebUtils.$$
@@ -26,8 +28,11 @@ import static com.ttpod.rest.groovy.CrudClosures.*
  * 2016/10/12
  * 用户特权列表
  */
-@Rest
+@RestWithSession
 class RedPacketApplyController extends BaseController {
+
+     static final Logger logger = LoggerFactory.getLogger(RedPacketApplyController.class)
+
     @Resource
     KGS productKGS
     /**
@@ -40,8 +45,10 @@ class RedPacketApplyController extends BaseController {
      */
     @Delegate
     Crud crud = new Crud(gameLogMongo.getCollection('red_packet_withdraw_conditions'), Boolean.TRUE,
-            [_id                  : {productKGS.nextId()}, desc: Str,day_apply_limit : Int, order: Int, withdraw_min_cash: Int,tariff:Str,
-             level_condition          : Int, status: Bool, timestamp: Timestamp, count_condition: Int],
+            [_id            : {
+                productKGS.nextId()
+            }, desc         : Str, day_apply_limit: Int, order: Int, withdraw_min_cash: Int, tariff: Str,
+             level_condition: Int, status: Bool, timestamp: Timestamp, count_condition: Int],
             new Crud.QueryCondition() {
                 public DBObject sortby(HttpServletRequest req) {
                     return new BasicDBObject('order', 1);
@@ -54,68 +61,82 @@ class RedPacketApplyController extends BaseController {
      * @param req
      * @return
      */
-//    def apply_logs(HttpServletRequest req){
-//        def log =gameLogMongo.getCollection('red_packet_apply_logs')
-//        def userId = ServletRequestUtils.getIntParameter(req,'user_id',0)
-//        def query = Web.fillTimeBetween(req)
-//        if(userId != 0){
-//            query.and('user_id').is(userId)
-//        }
-//        Crud.list(req, log, query.get(), ALL_FIELD, SJ_DESC)
-//    }
-//
-//    /**
-//     * 批量通过
-//     * @param req
-//     * @return
-//     */
-//    def batch_pass(HttpServletRequest req){
-//        List<Integer> applyIds = new ArrayList<Integer>(ids.length)
-//        def ids = req.getParameterValues('_ids')
-//        checkParamsValid(req,applyIds)
-//        if(ids.length == 0){
-//            return Web.missParam()
-//        }
-//        List<Integer> applyIds = new ArrayList<Integer>(ids.length)
-//        ids.each {
-//            it ->
-//                applyIds.add(Integer.valueOf(it.toString()))
-//        }
-//
-//        def apply_logs =gameLogMongo.getCollection('red_packet_apply_logs')
-//        def query = $$('_id':['$in':applyIds],'status':ApplyType.未处理.ordinal())
-//        def apply = apply_logs.find(query).toArray()
-//        if(apply.size() != ids.length){
-//            return Web.notAllowed()
-//        }
-//        def update = $$('status':ApplyType.通过.ordinal(),'last_modify':new Date().getTime())
-//        apply_logs.update(query,update,false,true,writeConcern)
-//    }
-//
-//    /**
-//     * 批量拒绝
-//     * @param req
-//     */
-//    def batch_refuse(HttpServletRequest req){
-//
-//    }
-//
-//    private Boolean checkParamsValid(HttpServletRequest req,applyIds){
-//        def ids = req.getParameterValues('_ids')
-//        if(ids.length == 0){
-//            return  Boolean.FALSE
-//        }
-//        ids.each {
-//            it ->
-//                applyIds.add(Integer.valueOf(it.toString()))
-//        }
-//
-//        def apply_logs =gameLogMongo.getCollection('red_packet_apply_logs')
-//        def query = $$('_id':['$in':applyIds],'status':ApplyType.未处理.ordinal())
-//        def apply = apply_logs.find(query).toArray()
-//        if(apply.size() != ids.length){
-//            return  Boolean.FALSE
-//        }
-//        return Boolean.TRUE
-//    }
+    def apply_logs(HttpServletRequest req) {
+        def log = gameLogMongo.getCollection('red_packet_apply_logs')
+        def userId = ServletRequestUtils.getIntParameter(req, 'user_id', 0)
+        def query = Web.fillTimeBetween(req)
+        if (userId != 0) {
+            query.and('user_id').is(userId)
+        }
+        def field = $$('match_condition':0)
+        Crud.list(req, log, query.get(),field , SJ_DESC)
+    }
+
+    /**
+     * 批量通过
+     * @param req
+     * @return
+     */
+    def batch_pass(HttpServletRequest req) {
+        def ids = req.getParameterValues('_ids')
+        if (ids.length == 0) {
+            return Web.missParam()
+        }
+
+        def apply_logs = gameLogMongo.getCollection('red_packet_apply_logs')
+        def query = $$('_id': ['$in': ids], 'status': ApplyType.未处理.ordinal())
+        def applyList = apply_logs.find(query).toArray()
+        if (applyList.size() != ids.length) {
+            return Web.notAllowed()
+        }
+        def update = $$('$set': $$('status': ApplyType.通过.ordinal(), 'last_modify': new Date().getTime()))
+        apply_logs.update(query, update, false, true, writeConcern)
+        return [code: 1]
+    }
+
+    /**
+     * 批量拒绝
+     * 拒绝要退钱
+     * @param req
+     */
+    def batch_refuse(HttpServletRequest req) {
+        def ids = req.getParameterValues('_ids')
+        if (ids.length == 0) {
+            return Web.missParam()
+        }
+
+        def apply_logs = gameLogMongo.getCollection('red_packet_apply_logs')
+        def query = $$('_id': ['$in': ids], 'status': ApplyType.未处理.ordinal())
+        def applyList = apply_logs.find(query).toArray()
+        if (applyList.size() != ids.length) {
+            return Web.notAllowed()
+        }
+
+        def apply_update = $$('$set': $$('status': ApplyType.未通过.ordinal(), 'last_modify': new Date().getTime()))
+        for (DBObject apply : applyList) {
+            def userId = apply['user_id'] as Integer
+            def income = apply['income'] as Long
+            def user_query = $$('_id': userId, 'status': Boolean.TRUE)
+            def user_update = $$('$inc': ['finance.cash_count': income])
+            if (users().update(user_query, user_update).getN() == 1) {
+                logger.debug('update users ok')
+                def applyId = apply['_id'].toString()
+                def apply_query = $$('_id': applyId, 'status': ApplyType.未处理.ordinal())
+                try {
+                    if(apply_logs.update(apply_query, apply_update).getN() == 0){
+                        logger.error('update apply error ')
+                        user_update = $$('$inc': ['finance.cash_count': -income])
+                        users().update(user_query, user_update)
+                    }
+                } catch (Exception e) {
+                    println("${e.printStackTrace()}")
+                    logger.error('update mongodb error ,error is {}', e.getMessage())
+                    user_update = $$('$inc': ['finance.cash_count': -income])
+                    users().update(user_query, user_update)
+                }
+            }
+        }
+        return [code: 1]
+    }
+
 }
