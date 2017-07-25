72a6146cf7f750da9cd5c36d2574f6653c15912c
jiao.li <jiao.li@2339.com>
2017-07-25 14:50:29 +0800

    user default avator

--- src/main/groovy/com/ttpod/star/admin/PhotoController.groovy
+++ src/main/groovy/com/ttpod/star/admin/PhotoController.groovy
@@ -102,7 +102,7 @@ class PhotoController extends BaseController {
     }
 
 
-    private final static String DEFAULT_PIC = "https://aiimg.sumeme.com/45/5/1487645253037.png";
+    private final static String DEFAULT_PIC = "https://aiimg.sumeme.com/49/1/1500964546481.png";
 
     def handle_user_pic(HttpServletRequest req) {
         Integer status = req['status'] as Integer
