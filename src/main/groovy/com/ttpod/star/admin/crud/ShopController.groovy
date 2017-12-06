445ac7c28fbd969d3becc38353b5e3b979595fa6
zhen.chen <zhen.chen@2339.com>
2017-12-06 15:51:54 +0800

    支付列表

--- src/main/groovy/com/ttpod/star/admin/crud/ShopController.groovy
+++ src/main/groovy/com/ttpod/star/admin/crud/ShopController.groovy
@@ -38,6 +38,7 @@ class ShopController extends BaseController{
     @Delegate Crud crud = new Crud(table(),true,
             [_id:{seqKGS.nextId()},item_id:Str,name:Str,pic:Str,status:Bool,cost:Int,count:Int,
              unit:Str,limit:Int,desc:Str,tag:Str,timestamp:Timestamp,lastModif:Timestamp,group:Str,
+             after_award_desc:Str,after_award_diamond:Int,after_award_days:Int,award_type:Int,
              stime:{String str->  (str == null || str.isEmpty()) ? null : Web.getTime(str).getTime()}
             ],
             new Crud.QueryCondition(){
